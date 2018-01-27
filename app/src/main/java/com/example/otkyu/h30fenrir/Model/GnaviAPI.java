9ea5707f34188471c8e2e265ae5494baa9276e5e
YukiOtake <djotkyu@gmail.com>
2018-01-27 10:52:12 +0900

    api result list get

--- app/src/main/java/com/example/otkyu/h30fenrir/Model/GnaviAPI.java
+++ app/src/main/java/com/example/otkyu/h30fenrir/Model/GnaviAPI.java
@@ -7,7 +7,9 @@ import android.os.AsyncTask;
 
 import java.net.URL;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import com.example.otkyu.h30fenrir.Model.Secret.AccessKey;
 import com.fasterxml.jackson.databind.*;
@@ -20,7 +22,19 @@ import com.fasterxml.jackson.databind.*;
  ******************************************************************************/
 
 public class GnaviAPI extends AsyncTask<String, String, String> {
-    private double[] gps = new double[2];
+    private double[] gps;
+    private static List<GnaviResultEntity> list;
+//    private static Integer requestNum;
+    private static boolean finishFlag;
+    private static boolean resultFlag;
+
+    public GnaviAPI() {
+        gps = new double[2];
+        list = new ArrayList<>();
+//        requestNum = 0;
+        finishFlag = false;
+        resultFlag = false;
+    }
 
     private void useApi() {
         // アクセスキー
@@ -29,19 +43,21 @@ public class GnaviAPI extends AsyncTask<String, String, String> {
         String acckey = accessKey.getKey();//please show "./Secret/readme.txt"
         // 緯度
 //        String lat = "35.670082";
-        String lat= String.valueOf(gps[0]);
+        String lat = String.valueOf(gps[0]);
 //        String lat = "37.358386";
         // 経度
 //        String lon = "139.763267";
-        String lon= String.valueOf(gps[1]);
+        String lon = String.valueOf(gps[1]);
 //        String lon = "140.383444";
-        System.out.println("!!!!GPS is " + gps[0] + ":" + gps[1]);
+//        System.out.println("!!!!GPS is " + gps[0] + ":" + gps[1]);
         // 範囲
-        String range = "5"; //大学の近くにお店がなかった orz
+        String range = "1"; //大学の近くにお店がなかった orz
         // 返却形式
         String format = "json";
         //出力数
         String hitPerPage = "20";
+//        requestNum = Integer.valueOf(hitPerPage);
+
         //ページ数
         String offsetPage = "1";
         // エンドポイント
@@ -67,7 +83,7 @@ public class GnaviAPI extends AsyncTask<String, String, String> {
         uri.append(prmHitPerPage);
         uri.append(prmOffsetPage);
 
-        System.out.println("url="+uri);
+        System.out.println("url=" + uri);
         // API実行、結果を取得し出力
         getNodeList(uri.toString());
     }
@@ -93,7 +109,13 @@ public class GnaviAPI extends AsyncTask<String, String, String> {
     private static void viewJsonNode(JsonNode nodeList) {
         if (nodeList != null) {
             //トータルヒット件数
-            String hitcount = "total:" + nodeList.path("total_hit_count").asText();
+            String total = nodeList.path("total_hit_count").asText();
+//            Integer total= Integer.valueOf(nodeList.path("total_hit_count").asText());
+            if (isCheckInteger(total)) {//数字にできるか判定
+                resultFlag = true;//検索結果はある！
+            }
+//            String hitcount = "total:" + nodeList.path("total_hit_count").asText();
+            String hitcount = "total:" + total;
             System.out.println(hitcount);
             //restのみ取得
             JsonNode restList = nodeList.path("rest");
@@ -101,20 +123,48 @@ public class GnaviAPI extends AsyncTask<String, String, String> {
             //店舗番号、店舗名、最寄の路線、最寄の駅、最寄駅から店までの時間、店舗の小業態を出力
             int count = 0;
             while (rest.hasNext()) {
+                GnaviResultEntity gnaviResultEntity = new GnaviResultEntity();
                 JsonNode r = rest.next();
                 String id = r.path("id").asText();
                 String name = r.path("name").asText();
                 String line = r.path("access").path("line").asText();
                 String station = r.path("access").path("station").asText();
                 String walk = r.path("access").path("walk").asText() + "分";
+                String address=r.path("address").asText();//住所
+                String nameKana=r.path("name_kana").asText();//ナマエ
+                String howGo=line+station+"から徒歩"+walk;//行き方
+                String tel=r.path("tel").asText();//電話番号
+                String opentime=r.path("opentime").asText();//営業時間
+                String[] img=new String[2];
+                img[0]=r.path("shop_image1").asText();
+                img[1]=r.path("shop_image2").asText();
                 String categorys = "";
-
                 for (JsonNode n : r.path("code").path("category_name_s")) {
                     categorys += n.asText();
                 }
                 System.out.println(id + "¥t" + name + "¥t" + line + "¥t" + station + "¥t" + walk + "¥t" + categorys + "count=" + count);
+                gnaviResultEntity.setName(name);
+                gnaviResultEntity.setAddress(address);
+                gnaviResultEntity.setNameKana(nameKana);
+                gnaviResultEntity.setOpentime(opentime);
+                gnaviResultEntity.setTel(tel);
+                gnaviResultEntity.setHowGo(howGo);
+                gnaviResultEntity.setImg(img);
+                list.add(gnaviResultEntity);
+//                list.get(count).setName(name);
+                System.out.println(count+":"+gnaviResultEntity.getName());
                 count++;
             }
+            finishFlag = true;
+        }
+    }
+
+    private static boolean isCheckInteger(String a) {//数字にできるか判定
+        try {
+            Integer hoge = Integer.valueOf(a);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 
@@ -126,5 +176,20 @@ public class GnaviAPI extends AsyncTask<String, String, String> {
 
     public void setGps(double[] gps) {
         this.gps = gps;
+    }
+
+    public static List<GnaviResultEntity> getList() {
+//        for(int i=0;i<list.size();i++){
+//            System.out.println("list "+i+"="+list.get(i).getName());
+//        }
+        return list;
+    }
+
+    public static boolean isFinishFlag() {
+        return finishFlag;
+    }
+
+    public static boolean isResultFlag() {
+        return resultFlag;
     }
 }
