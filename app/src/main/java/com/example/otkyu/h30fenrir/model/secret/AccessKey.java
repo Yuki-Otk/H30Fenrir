b184fa962144356dee0050775b48e3a7db0d595f
YukiOtake <djotkyu@gmail.com>
2018-01-27 12:31:14 +0900

    input your accesskey set for AccessKey.java

---
+++ app/src/main/java/com/example/otkyu/h30fenrir/model/secret/AccessKey.java
@@ -0,0 +1,13 @@
+package com.example.otkyu.h30fenrir.model.secret;
+
+/**
+ * Created by YukiOtake on 2018/01/27 027.
+ */
+
+public class AccessKey {
+    private String key="input your accesskey";
+
+    public String getKey() {
+        return key;
+    }
+}
