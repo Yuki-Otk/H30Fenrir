7be6b913b348dc4b6898ae0d9688f52302b4487d
YukiOtake <djotkyu@gmail.com>
2018-01-28 23:42:06 +0900

    make layout commandbutton

--- app/src/main/java/com/example/otkyu/h30fenrir/ShowDetailsActivity.java
+++ app/src/main/java/com/example/otkyu/h30fenrir/ShowDetailsActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.otkyu.h30fenrir.model.GnaviAPI;
@@ -22,6 +24,7 @@ public class ShowDetailsActivity  extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details_show);
+
 
         Button backButton = findViewById(R.id.back_button);
         backButton.setOnClickListener(new View.OnClickListener() {
